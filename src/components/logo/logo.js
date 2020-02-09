59f44fa324751d29fd4d3604fa97c4540e1a3f4e
Justin Leatherwood <2601724+jplwood@users.noreply.github.com>
2020-02-09 15:04:47 -0500

    logo looking decent

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -4,19 +4,18 @@ import "./logo.scss";
 
 function Logo({ children }) {
   return (
-    <div className="logo">
-      <a href="{{ '/' | prepend: site.baseurl }}">
-        <img
-          className="norseman-plane"
-          src="/images/norseman_aircraft_yr.png"
-          alt="norseman aircraft"
-        />
-        <h1>
-          <span>Norseman Group</span>
-          <span>Life Settlements</span>
-        </h1>
-      </a>
-    </div>
+    <a className="logo">
+      {/* TODO REPLACE WITH NEXT LINK */}
+      <img
+        className="logo__img"
+        src="/images/norseman_aircraft_yr.png"
+        alt="norseman aircraft"
+      />
+      <div className="logo__text">
+        <div className="logo__text--smaller">Norseman Group</div>
+        <div>Life Settlements</div>
+      </div>
+    </a>
   );
 }
 
